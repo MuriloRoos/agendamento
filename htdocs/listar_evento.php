34ed8a1c357a6f0aa7c93f86e3cf0313714257d4
MuriloRoos <105290826+MuriloRoos@users.noreply.github.com>
2024-08-29 09:19:56 -0300

    Update listar_evento.php

--- htdocs/listar_evento.php
+++ htdocs/listar_evento.php
@@ -30,3 +30,39 @@
         ]
     }
 ?>
+
+<?php
+
+// Incluir o arquivo com a conexão com banco de dados
+include_once 'conexao.php';
+
+// QUERY para recuperar os eventos
+$query_events = "SELECT id, title, color, start, end FROM events";
+
+// Prepara a query
+$result_events = $conn->prepare($query_events);
+
+// Executa a query
+$result_events->execute();
+
+// Cria o array que recebe os eventos
+$events = [];
+
+// Pega o resultado da query
+$result = $result_events->get_result();
+
+// Percorre a lista de registros retornado do banco de dados
+while ($row_events = $result->fetch_assoc()) {
+    // Extrai o array
+    extract($row_events);
+
+    $events[] = [
+        'id' => $id,
+        'title' => $title,
+        'color' => $color,
+        'start' => $start,
+        'end' => $end,
+    ];
+}
+
+?>
